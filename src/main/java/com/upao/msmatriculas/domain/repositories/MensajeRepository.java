0126c4f3fb32fc8b98660ec874a771c4b828ce84
PavelUzategui <pusateguir1@upao.edu.pe>
2022-12-16 19:48:57 -0500

    Commit 1

---
+++ src/main/java/com/upao/msmatriculas/domain/repositories/MensajeRepository.java
@@ -0,0 +1,15 @@
+package com.upao.msmatriculas.domain.repositories;
+
+import com.upao.msmatriculas.domain.entities.Mensaje;
+
+import java.util.List;
+
+public interface MensajeRepository {
+    boolean Registrar(Mensaje mensaje);
+    List<Mensaje> ListarTodos();
+
+    List<Mensaje> ListarxRemitente(String remitente);
+
+    boolean Borrar(Mensaje mensaje);
+
+}
---
+++ src/main/java/com/upao/msmatriculas/domain/services/MensajeService.java
@@ -0,0 +1,17 @@
+package com.upao.msmatriculas.domain.services;
+
+import com.upao.msmatriculas.domain.entities.Mensaje;
+
+import java.util.List;
+
+public interface MensajeService {
+    boolean Registrar(Mensaje mensaje);
+
+    boolean Borrar(Mensaje mensaje);
+
+    List<Mensaje> ListarTodos();
+
+    List<Mensaje> ListarxRemitente(String remitente);
+
+
+}
